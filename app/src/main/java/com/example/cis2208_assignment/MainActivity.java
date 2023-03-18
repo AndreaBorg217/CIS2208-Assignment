9e5a346c33c371aaeb603af714db71703cbadb81
AndreaBorg217 <andreaborg217@gmail.com>
2023-03-18 13:26:17 +0100

    Created UI layout for MainActivity in Landscape

--- app/src/main/java/com/example/cis2208_assignment/MainActivity.java
+++ app/src/main/java/com/example/cis2208_assignment/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.cis2208_assignment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,10 +10,26 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
+            setContentView(R.layout.activity_main_landscape);
+        } else{
+            setContentView(R.layout.activity_main_portrait);
+        }
 
         if (getSupportActionBar() != null) {
             getSupportActionBar().hide();
+        }
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        // Reload the appropriate layout when device orientation changes
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            setContentView(R.layout.activity_main_landscape);
+        } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT){
+            setContentView(R.layout.activity_main_portrait);
         }
     }
 }
