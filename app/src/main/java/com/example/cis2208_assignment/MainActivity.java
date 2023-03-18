eb7c41fb33bf82ebb09d895e6fcea26a40c14fc9
AndreaBorg217 <andreaborg217@gmail.com>
2023-03-18 11:53:50 +0100

    Initialisation

---
+++ app/src/main/java/com/example/cis2208_assignment/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.cis2208_assignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
