63c5a80e5d2b488ffe1a0bcb71bfbc1345c9cbfe
AndreaBorg217 <andreaborg217@gmail.com>
2023-03-30 08:45:08 +0200

    Added Profile page layout

---
+++ app/src/main/java/com/example/cis2208_assignment/Profile.java
@@ -0,0 +1,18 @@
+package com.example.cis2208_assignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+    }
+}
