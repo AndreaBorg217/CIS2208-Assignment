876a58f9c9a6612128ee631b66142e22ae6e4646
AndreaBorg217 <andreaborg217@gmail.com>
2023-04-04 14:29:20 +0200

    Added land. layout for CategoryScores

--- app/src/main/java/com/example/cis2208_assignment/CategoryScores.java
+++ app/src/main/java/com/example/cis2208_assignment/CategoryScores.java
@@ -2,6 +2,7 @@ package com.example.cis2208_assignment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 
 public class CategoryScores extends AppCompatActivity {
@@ -9,11 +10,26 @@ public class CategoryScores extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_category_scores);
+
+        if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
+            setContentView(R.layout.activity_category_scores_landscape);
+        } else{
+            setContentView(R.layout.activity_category_scores_portrait);
+        }
 
         if (getSupportActionBar() != null) {
             getSupportActionBar().hide();
         }
     }
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
 
+        // Reload the appropriate layout when device orientation changes
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            setContentView(R.layout.activity_main_landscape);
+        } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT){
+            setContentView(R.layout.activity_main_portrait);
+        }
+    }
 }
